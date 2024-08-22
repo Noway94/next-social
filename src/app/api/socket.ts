bf83195a4b880b79f09453f6879a4c244c1ea723
Noway94 <hamolol@hotmail.com>
2024-08-22 18:48:46 +0300

    messages page added

--- src/app/api/socket.ts
+++ src/app/api/socket.ts
@@ -3,7 +3,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Server as HTTPServer } from 'http';
 import { Socket as NetSocket } from 'net';
 
-export const runtime = 'nodejs'; // New format for the config
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 type NextApiResponseWithSocket = NextApiResponse & {
   socket: NetSocket & {
@@ -21,26 +25,25 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
     });
 
     io.on('connection', (socket) => {
-     // console.log('New client connected:', socket.id);
+      console.log('New client connected:', socket.id);
 
       socket.on('sendMessage', (message) => {
-     //   console.log('Message received:', message);
+        console.log('Message received:', message);
         io.emit('receiveMessage', message);
       });
 
       socket.on('typing', (data) => {
         socket.broadcast.emit('typing', data);
       });
-  
+
       socket.on('callUser', (data) => {
-        io.to(data.userToCall).emit('callUser',
-           { signal: data.signal, from: data.from });
+        io.to(data.userToCall).emit('callUser', { signal: data.signal, from: data.from });
       });
-    
+
       socket.on('answerCall', (data) => {
         io.to(data.to).emit('callAccepted', data.signal);
       });
-      
+
       socket.on('disconnect', () => {
         console.log('Client disconnected:', socket.id);
       });
